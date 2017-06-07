a910f1e2d4925f3bc20fe96bba6f13aaf52d530d
akamornikov <akamornikov@gmail.com>
2017-06-07 16:55:41 +0300

    Eureka introduction

--- recommendations-service/src/main/java/com/sg/recommendations/Application.java
+++ recommendations-service/src/main/java/com/sg/recommendations/Application.java
@@ -2,11 +2,13 @@ package com.sg.recommendations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
